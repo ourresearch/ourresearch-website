8f615acc8325f436364b17e8f6074b2e7ac96e5c
Jason Priem <jp@jasonpriem.org>
2019-06-11 01:41:05 -0700

    products tweaks.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -23,7 +23,7 @@
         </section>
 
 
-        <section style="padding: 50px 0;">
+        <section style="padding: 50px 0 0 0;">
             <v-container fluid class="px-0 pt-0 pb-2" style="background:#555;">
                 <v-layout row align-center class="py-4">
                     <v-flex class="text-xs-center display-1 font-weight-light white--text">These are our values:
@@ -51,89 +51,94 @@
 
         </section>
 
-
-        <v-container class="py-5 my-5">
-            <v-layout row align-center>
-                <v-flex class="display-2 text-xs-center">This is what we build:</v-flex>
-            </v-layout>
-            <v-container fluid grid-list-lg>
-                <v-layout wrap justify-center>
-                    <v-flex class="my-card" xs12 sm6>
-                        <v-card>
-                            <v-layout fill-height>
-                                <v-flex xs4 style="background: #42a0f2">
-                                    <v-container fill-height>
-                                        <v-layout align-center justify-center>
-                                            <div>depsy</div>
-                                        </v-layout>
-                                    </v-container>
-                                </v-flex>
-                                <v-flex xs8>
-                                    Lorem ipsum dolor amet sriracha pug distillery chicharrones banh mi, next level tofu edison bulb gluten-free portland vice art party heirloom venmo chia. Locavore yuccie live-edge, green juice cold-pressed pickled mixtape taiyaki beard air plant pabst unicorn biodiesel truffaut gentrify. Single-origin coffee letterpress green juice schlitz enamel pin hot chicken kombucha celiac leggings. Swag etsy before they sold out, blog kogi chicharrones pop-up meh live-edge jean shorts portland seitan. Trust fund palo santo pug, man braid kogi craft beer bitters williamsburg kinfolk locavore hoodie.
-
-Whatever waistcoat scenester lyft locavore thundercats. Marfa vaporware bicycle rights pinterest kogi roof party. Selfies enamel pin post-ironic twee 90's vexillologist. Before they sold out messenger bag pug, church-key austin knausgaard prism paleo man bun chillwave narwhal. Drinking vinegar scenester swag, pabst cardigan umami air plant try-hard vinyl. Affogato farm-to-table direct trade irony, wayfarers raclette seitan ramps pork belly small batch scenester.
+        <section style="background: #fafafa;">
 
 
-                                </v-flex>
-                            </v-layout>
-
-                        </v-card>
-
-                    </v-flex>
+            <v-container class="py-5" >
 
 
-
-
-
-
-<!--                    <v-flex sm12 md3>-->
-<!--                        <v-card>-->
-<!--                            <v-img src="https://i.imgur.com/05HieLu.png"></v-img>-->
-<!--                            <v-card-text>Lorem ipsum dolor amet pok pok normcore seitan umami, pickled freegan iceland-->
-<!--                                asymmetrical crucifix jianbing.-->
-<!--                            </v-card-text>-->
-<!--                        </v-card>-->
-
-<!--                    </v-flex>-->
-
-
-<!--                    <v-flex sm12 md3>-->
-<!--                        <v-card>-->
-<!--                            <v-img src="https://i.imgur.com/xLwYhap.png"></v-img>-->
-<!--                            <v-card-text>Lorem ipsum dolor amet pok pok normcore seitan umami, pickled freegan iceland-->
-<!--                                asymmetrical crucifix jianbing.-->
-<!--                            </v-card-text>-->
-
-<!--                        </v-card>-->
-
-<!--                    </v-flex>-->
-
-<!--                    <v-flex sm12 md3>-->
-<!--                        <v-card>-->
-<!--                            <v-img src="https://i.imgur.com/4WLDRPI.png"></v-img>-->
-<!--                            <v-card-text>Lorem ipsum dolor amet pok pok normcore seitan umami, pickled freegan iceland-->
-<!--                                asymmetrical crucifix jianbing.-->
-<!--                            </v-card-text>-->
-
-<!--                        </v-card>-->
-<!--                    </v-flex>-->
-
-<!--                    <v-flex sm12 md3>-->
-<!--                        <v-card>-->
-<!--                            <v-img src="https://i.imgur.com/xFLO2H5.png"></v-img>-->
-<!--                            <v-card-text>Lorem ipsum dolor amet pok pok normcore seitan umami, pickled freegan iceland-->
-<!--                                asymmetrical crucifix jianbing.-->
-<!--                            </v-card-text>-->
-
-<!--                        </v-card>-->
-<!--                    </v-flex>-->
-
-
+                <v-layout row align-center>
+                    <v-flex class="display-2 text-xs-center">This is what we build:</v-flex>
                 </v-layout>
+                <v-container fluid grid-list-lg>
+                    <v-layout wrap justify-center>
+                        <v-flex class="my-card" xs12 sm6>
+                            <v-card>
+                                <v-layout fill-height>
+                                    <v-flex xs4 style="background: #42a0f2">
+                                        <v-container fill-height>
+                                            <v-layout align-center justify-center>
+                                                <div>depsy</div>
+                                            </v-layout>
+                                        </v-container>
+                                    </v-flex>
+                                    <v-flex xs8>
+                                        Lorem ipsum dolor amet sriracha pug distillery chicharrones banh mi, next level tofu edison bulb gluten-free portland vice art party heirloom venmo chia. Locavore yuccie live-edge, green juice cold-pressed pickled mixtape taiyaki beard air plant pabst unicorn biodiesel truffaut gentrify. Single-origin coffee letterpress green juice schlitz enamel pin hot chicken kombucha celiac leggings. Swag etsy before they sold out, blog kogi chicharrones pop-up meh live-edge jean shorts portland seitan. Trust fund palo santo pug, man braid kogi craft beer bitters williamsburg kinfolk locavore hoodie.
+
+    Whatever waistcoat scenester lyft locavore thundercats. Marfa vaporware bicycle rights pinterest kogi roof party. Selfies enamel pin post-ironic twee 90's vexillologist. Before they sold out messenger bag pug, church-key austin knausgaard prism paleo man bun chillwave narwhal. Drinking vinegar scenester swag, pabst cardigan umami air plant try-hard vinyl. Affogato farm-to-table direct trade irony, wayfarers raclette seitan ramps pork belly small batch scenester.
+
+
+                                    </v-flex>
+                                </v-layout>
+
+                            </v-card>
+
+                        </v-flex>
+
+
+
+
+
+
+    <!--                    <v-flex sm12 md3>-->
+    <!--                        <v-card>-->
+    <!--                            <v-img src="https://i.imgur.com/05HieLu.png"></v-img>-->
+    <!--                            <v-card-text>Lorem ipsum dolor amet pok pok normcore seitan umami, pickled freegan iceland-->
+    <!--                                asymmetrical crucifix jianbing.-->
+    <!--                            </v-card-text>-->
+    <!--                        </v-card>-->
+
+    <!--                    </v-flex>-->
+
+
+    <!--                    <v-flex sm12 md3>-->
+    <!--                        <v-card>-->
+    <!--                            <v-img src="https://i.imgur.com/xLwYhap.png"></v-img>-->
+    <!--                            <v-card-text>Lorem ipsum dolor amet pok pok normcore seitan umami, pickled freegan iceland-->
+    <!--                                asymmetrical crucifix jianbing.-->
+    <!--                            </v-card-text>-->
+
+    <!--                        </v-card>-->
+
+    <!--                    </v-flex>-->
+
+    <!--                    <v-flex sm12 md3>-->
+    <!--                        <v-card>-->
+    <!--                            <v-img src="https://i.imgur.com/4WLDRPI.png"></v-img>-->
+    <!--                            <v-card-text>Lorem ipsum dolor amet pok pok normcore seitan umami, pickled freegan iceland-->
+    <!--                                asymmetrical crucifix jianbing.-->
+    <!--                            </v-card-text>-->
+
+    <!--                        </v-card>-->
+    <!--                    </v-flex>-->
+
+    <!--                    <v-flex sm12 md3>-->
+    <!--                        <v-card>-->
+    <!--                            <v-img src="https://i.imgur.com/xFLO2H5.png"></v-img>-->
+    <!--                            <v-card-text>Lorem ipsum dolor amet pok pok normcore seitan umami, pickled freegan iceland-->
+    <!--                                asymmetrical crucifix jianbing.-->
+    <!--                            </v-card-text>-->
+
+    <!--                        </v-card>-->
+    <!--                    </v-flex>-->
+
+
+                    </v-layout>
+
+                </v-container>
 
             </v-container>
-
-        </v-container>
+        </section>
 
 
         <section style="height: 100vh">
